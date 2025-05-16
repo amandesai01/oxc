80b7b0aa3a2b8fee569062f421ab7e221cbf0dde
overlookmotel <theoverlookmotel@gmail.com>
2025-05-16 18:32:12 +0100

    feat(allocator): replace `bumpalo`

---
+++ crates/oxc_allocator/src/arena/mod.rs
@@ -0,0 +1,22 @@
+mod alloc_trait;
+mod allocator_api2;
+#[expect(clippy::module_inception)]
+mod arena;
+mod config;
+mod constants;
+mod footer;
+
+pub use alloc_trait::Alloc;
+pub use arena::Arena;
+pub use config::{ArenaConfig, ArenaConfigDefault, ArenaConfigPointerAligned};
+pub use constants::{CHUNK_ALIGN, MAX_INITIAL_CAPACITY};
+
+/// Default [`Arena`], with `MIN_ALIGN = 1`.
+///
+/// i.e. all types will be stored with only their required alignment.
+pub type ArenaDefault = Arena<ArenaConfigDefault>;
+
+/// [`Arena`] with `MIN_ALIGN = align_of::<usize>()`.
+///
+/// i.e. all types will be stored with pointer alignment (or greater if the type requires it).
+pub type ArenaPointerAligned = Arena<ArenaConfigPointerAligned>;
